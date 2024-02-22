f07e59091f17dccb7363d5d8cc7b13ad73ef0035
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2024-02-22 22:27:00 +0100

    add interface example

---
+++ interface-example/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+)
+
+type MySlowReader struct {
+	content  string
+	position int
+}
+
+func (m *MySlowReader) Read(p []byte) (n int, err error) {
+	if m.position+1 <= len(m.content) {
+		n := copy(p, m.content[m.position:m.position+1])
+		m.position++
+		return n, nil
+	}
+	return 0, io.EOF
+}
+
+func main() {
+
+	mySlowReaderInstance := &MySlowReader{content: "Hello World!"}
+
+	out, err := io.ReadAll(mySlowReaderInstance)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("output: %s\n", out)
+}
