682e0a98a6714154b1bd0ac28f4e864c4b5280dc
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2024-11-07 22:18:52 +0100

    add k8 deployment

---
+++ kubernetes/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	var (
+		client           *kubernetes.Clientset
+		deploymentLabels map[string]string
+		err              error
+	)
+
+	if client, err = getClient(); err != nil {
+		fmt.Printf("Error: %s", err)
+		os.Exit(1)
+	}
+
+	if deploymentLabels, err = deploy(context.Background(), client); err != nil {
+		fmt.Printf("Error: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Deploy finished. Did a deploy with labels: %+v", deploymentLabels)
+}
+
+func getClient() (*kubernetes.Clientset, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return clientset, nil
+
+}
+
+func deploy(ctx context.Context, client *kubernetes.Clientset) (map[string]string, error) {
+	var deployment *v1.Deployment
+
+	appFile, err := os.ReadFile("app.yaml")
+	if err != nil {
+		return nil, fmt.Errorf("readfile error: %s", err)
+	}
+
+	obj, groupVersionKind, err := scheme.Codecs.UniversalDeserializer().Decode(appFile, nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("deserialize error: %s", err)
+	}
+
+	switch obj := obj.(type) {
+	case *v1.Deployment:
+		deployment = obj
+	default:
+		return nil, fmt.Errorf("unrecognized type: %s", groupVersionKind)
+	}
+
+	res, err := client.AppsV1().Deployments("default").Create(ctx, deployment, metav1.CreateOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("deployment failed: %s", err)
+	}
+
+	return res.Spec.Template.Labels, nil
+}
