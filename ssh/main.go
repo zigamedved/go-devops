2f59f9d8db65de8f5bc455ed56694adbedb61e3a
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2024-11-08 21:47:17 +0100

    move keys

--- ssh/main.go
+++ ssh/main.go
@@ -17,12 +17,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = os.WriteFile("mykey.pem", privateKey, 0600); err != nil {
+	if err = os.WriteFile("./keys/mykey.pem", privateKey, 0600); err != nil {
 		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
 
-	if err = os.WriteFile("mykey.pub", publicKey, 0600); err != nil {
+	if err = os.WriteFile("./keys/mykey.pub", publicKey, 0600); err != nil {
 		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
